de799dce97dd1d3279131634db41e4b63a79dd5e
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-08-13 18:01:40 +0000

    test the existance of keys in game object

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+let game = {
+    "score": 0,
+    "currentGame": [],
+    "playerMove": [],
+    "choices": [],
+}
+
+
+
+module.exports = {game};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,4 +2,30 @@
  * @jest-environment jsdom
  */
 
+const {game} = require("../game")
+
+
+beforeAll(() => {
+    const fs = require("fs");
+    let htmlPageContent = fs.appendFileSync("index.html", "utf-8");
+    document.open();
+    document.write(htmlPageContent);
+    document.close();
+
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMove key exists", () => {
+        expect("playerMove" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+});
 
